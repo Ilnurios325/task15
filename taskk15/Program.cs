1dc47cd8296d1863bcc347f92cd65273c4780034
Ilnurios325 <ipilulka@gmail.com>
2023-01-08 09:58:45 +0300

    solt15

---
+++ taskk15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("input a digitwhich means a day of week");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 | num == 7){
+    Console.WriteLine("weekend");
+}
+else
+    Console.WriteLine("not weekend");
+    
